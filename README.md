9e474f702f3fc8074c5539a30c328d695bd15b07
QuynhTaVo <quynhtavo@gmail.com>
2022-09-24 15:17:04 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fictional-doodle
+Another project
+Libraries: pandas, sqlalchemy, nltk, sklearn, pickle, flask
+Files:
+- process_data: ETL
+- train_classifier: ML model
+- run: web app
+Acknowledgments: not to Udacity
---
+++ train_classifier.py
@@ -0,0 +1,83 @@
+#GET_PASSES_THIS_REPO_UDACITY_PLEASE
+# import libraries
+import sys
+from sqlalchemy import create_engine
+import pandas as pd
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+def load_data(database_filepath):
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql('SELECT * FROM disaster_categories', engine)
+    X = df['message']
+    y = df.iloc[:,4:]
+    y.drop(columns=['related', 'child_alone'], inplace=True)
+    return X, y, y.columns
+
+def tokenize(text):
+    text = text.lower()
+    tokens = word_tokenize(text)
+    tokens = [w for w in tokens if w not in stopwords.words("english")]
+    tokens = [WordNetLemmatizer().lemmatize(w) for w in tokens]
+    return tokens
+
+
+def build_model():
+    pipeline = Pipeline([('CountVectorizer', CountVectorizer(tokenizer=tokenize)), ('TfidfTransformer', TfidfTransformer()), ('MultiOutputClassifier', MultiOutputClassifier(RandomForestClassifier()))])
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    print(classification_report(Y_test, y_pred, target_names=category_names))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
